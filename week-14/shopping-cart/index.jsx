dd8769da99897f17b461522819f96130a2f69bdf
Nick Laptop <45341924+nitnub@users.noreply.github.com>
2022-07-30 13:49:01 -0400

    finalized week 14 requirements

--- week-14/shopping-cart/index.jsx
+++ week-14/shopping-cart/index.jsx
@@ -1,17 +1,51 @@
 // Ex 2 - remove any item from navbar with less than minStock in stock
 // write out both the name and the number in stock in format apple:2
 function NavBar({ menuitems, minstock }) {
-  const filteredList = menuitems.filter((item) => item.instock > minstock);
+  const [cart, setCart] = React.useState([]);
+  const [stock, setStock] = React.useState(menuitems);
 
-  const updatedList = filteredList.map((item, index) => {
+  const { Button } = ReactBootstrap;
+
+  const moveToCart = (e) => {
+    const [name, count] = e.target.innerHTML.split(': ');
+    let newStock;
+
+    if (count > minstock) {
+      newStock = stock.map((item, index) => {
+        if (item.name === name) {
+          item.instock--;
+
+          console.log(item);
+          const newCart = [...cart, item];
+
+          setCart(newCart);
+        }
+        return item;
+      });
+      setStock(newStock);
+    }
+  };
+
+  const updatedList = menuitems.map((item, index) => {
     return (
-      <li key={index}>
-        {item.name} : {item.instock}
-      </li>
+      <Button key={index} onClick={moveToCart}>
+        {item.name}: {item.instock}
+      </Button>
     );
   });
-  // note that React needs to have a single Parent
-  return <ul style={{ listStyleType: 'none' }}>{updatedList}</ul>;
+
+  const cartDisplay = cart.map((item, index) => {
+    return <Button key={`cart.${index}`}>{item.name}</Button>;
+  });
+
+  return (
+    <>
+      <h1>React Lists</h1>
+      <ul style={{ listStyleType: 'none' }}>{updatedList}</ul>;
+      <h1>Shopping Cart</h1>
+      <ul style={{ listStyleType: 'none' }}>{cartDisplay}</ul>;
+    </>
+  );
 }
 const menuItems = [
   { name: 'apple', instock: 2 },
@@ -22,4 +56,4 @@ const menuItems = [
 ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<NavBar menuitems={menuItems} minstock={2} />);
+root.render(<NavBar menuitems={menuItems} minstock={0} />);
